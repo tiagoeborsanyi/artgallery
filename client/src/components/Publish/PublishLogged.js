725b2ae1c587bc3645a9703dc5003c06a8e41694
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-07-07 16:48:35 -0300

    create component InputImage

--- client/src/components/Publish/PublishLogged.js
+++ client/src/components/Publish/PublishLogged.js
@@ -1,52 +1,52 @@
 import React from 'react'
 
 import './PublishLogged.css'
+import Input from '../UI/Input/Input'
+import InputImage from '../UI/InputImage/InputImage'
 
 const PublishLogged = props => {
+  const presKey = e => {
+    if(e.key === 'Enter') {
+      console.log(e.target.value)
+    }
+  }
+
   return (
     <React.Fragment>
       <div className="container">
         <h1 className="container-newart-title">New Art</h1>
         <div className="container-newart">
-          <form>
-            <div className="newart-form-control space">
-              <label>Title</label>
-              <input type="text" />
-            </div>
+          <form onSubmit={e => e.preventDefault()}>
+            <Input
+              classes="space"
+              label="Title"
+              value=""
+              changed={() => {}}
+              // Include element configs
+               />
             <div className="newart-form-control-flexline space">
-              <div className="newart-form-control input-file space">
-                <label>Thumbnail <span>280x350</span></label>
-                <div className="image-preview">
-                  <input className="image-preview__input" type="file" accept=".jpg,.png,.jpeg" />
-                    <div className="image-preview__img"></div>
-                    <div className="image-preview__change">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none" /><path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z" /></svg>
-                      <span>Choose File...</span>
-                    </div>
-                    <div className="image-preview__hover"></div>
-                </div>
-              </div>
+              <InputImage
+                addClassFormControl="input-file"
+                title="Thumbnail"
+                subtitle="280x350"
+                height="175px"
+                width="140px" />
               <div className="newart-form-control textarea space">
                 <label>Description</label>
                 <textarea></textarea>
               </div>
             </div>
-            <div className="newart-form-control original-image space">
-              <label>Original Image <span>(Original size image)</span></label>
-              <div className="original-image__preview">
-                <input className="original-image__input" type="file" accept=".jpg,.png,.jpeg" />
-                  <div className="original-image__img"></div>
-                  <div className="original-image__change">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="100px" height="100px"><path d="M0 0h24v24H0z" fill="none" /><path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z" /></svg>
-                    <span>Choose File...</span>
-                  </div>
-                  <div className="original-image__hover"></div>
-              </div>
-            </div>
+            <InputImage
+              title="Original Image"
+              subtitle="(Original size image)"
+              svgWidth="100px"
+              svgHeight="100px"
+              height="270px"
+              width="220px" />
             <div className="newart-tags">
               <div className="newart-form-control space">
                 <label>Tags <span>(Optional)</span></label>
-                <input type="text" placeholder="Press 'Enter' after each tag." />
+                <input type="text" placeholder="Press 'Enter' after each tag." onKeyDown={presKey} />
               </div>
                 <ul className="newart-tags-result__items">
                   <li className="newart-tags-result__item">
---
+++ client/src/components/UI/InputImage/InputImage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './InputImage.css'
+
+const InputImage = props => {
+  return (
+    <div className={`newart-form-control space ${props.addClassFormControl}`}>
+      <label>{props.title} <span>{props.subtitle}</span></label>
+      <div className="newart-image__preview" style={{width: props.width, height: props.height}}>
+        <input className="newart-image__input" style={{width: props.width, height: props.height}} type="file" accept=".jpg,.png,.jpeg" />
+          <div className="newart-image__img" style={{width: props.width, height: props.height, backgroundImage: `url(${props.imagePreviewURL})`}}></div>
+          <div className="newart-image__change" style={{width: props.width, height: props.height}}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width={props.svgWidth ? props.svgWidth : "48px"} height={props.svgHeight ? props.svgHeight : "48px"}><path d="M0 0h24v24H0z" fill="none" /><path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z" /></svg>
+            <span>Choose File...</span>
+          </div>
+          <div className="newart-image__hover" style={{width: props.width, height: props.height}}></div>
+      </div>
+    </div>
+  )
+}
+
+export default InputImage
