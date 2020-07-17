1adaf0f88938c53d5931f7bc70e5e10fb2619899
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-07-17 12:11:40 -0300

    create simple arte reducer

--- client/src/containers/ViewImage/ViewImage.js
+++
@@ -1,9 +0,0 @@
-import React from  'react'
-
-import './ViewImage'
-
-const ViewImage = props => {
-  return <div className='viewimage'>View Image</div>
-}
-
-export default ViewImage
--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,13 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import authReducer from './store/reducers/auth'
+import arteReducer from './store/reducers/arte'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  arte: arteReducer
 })
 
 const store = createStore(rootReducer, composeEnhancers(
