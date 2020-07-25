4a3a25d2384b6dacf264113fa7d05c073ee679d2
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-07-25 13:15:11 -0300

    work on delete function comment

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,6 +1,8 @@
 const HttpError = require('../models/http-error')
 const User = require('../models/user')
 const Photo = require('../models/photo')
+const user = require('../models/user')
+const photo = require('../models/photo')
 
 // ADD COMMENT
 const addComment = async (req, res, next) => {
@@ -40,8 +42,31 @@ const addComment = async (req, res, next) => {
 }
 
 // DELETE COMMENT
-const deleteComment = (req, res, next) => {
+const deleteComment = async (req, res, next) => {
+  const { pid, cid } = req.params
+  const { uid } = req.body
+  let user
+  let photo
 
+  try {
+    user = await User.findOne({ uid })
+  } catch(error) {
+    const err = new HttpError('Delete comment failed, for search user.', 500)
+    return next(err)
+  }
+
+  try {
+    photo = await Photo.findOne({ _id: pid })
+  } catch(error) {
+    const err = new HttpError('Delete comment failed, for search arte.', 500)
+    return next(err)
+  }
+
+  if (user._id.toString() !== cid) {
+    const err = new HttpError('Delete comment failed.', 500)
+    return next(err)
+  }
+  res.status(201).json({success: 'ok'})
 }
 
 exports.addComment = addComment
