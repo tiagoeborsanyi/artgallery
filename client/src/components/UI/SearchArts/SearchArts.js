bac41068b9fcfea855842518bf777ee455663c60
Tiago Emerick <tiagoeborsanyi@gmail.com>
2020-08-25 18:12:54 -0300

    create state to button searchArts component

--- client/src/components/UI/SearchArts/SearchArts.js
+++ client/src/components/UI/SearchArts/SearchArts.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 import './SearchArts.css'
 
 const SearchArts = props => {
+  const [vFilter, setVFilter] = useState(false)
+  let wrapper = useRef(false)
+
+  useEffect(() => {
+    document.addEventListener('click', () => {
+      if (wrapper.current && vFilter) {
+        setVFilter(false)
+      }
+    })
+    return () => {
+      wrapper.current = true
+    }
+  }, [wrapper, vFilter])
+
   return (
     <div className="search-arts">
       <div className="container-search-arts">
-        <button className="search-arts__filter-button">
+        <button
+          className="search-arts__filter-button"
+          onClick={() => {
+            if(!!vFilter) {
+              setVFilter(false)
+              return;
+            }
+            setVFilter(true)
+            wrapper.current = false;
+          }}>
             Filter Arts
             <span className="material-icons">
                 expand_more
             </span>
         </button>
-        <div className="search-arts__filter-list">
+        <div
+          className="search-arts__filter-list"
+          style={{display: vFilter ? 'block' : ''}}>
             <ul className="search-arts__filter-list__items">
                 <li className="search-arts__filter-list__item">
                     <button>Arts</button>
